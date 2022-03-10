888120c09236e600c0168074364ee8ff54f07c77
ShubhamSatnurkar <shubhamsatnurkar061@gmail.com>
2022-03-10 16:34:43 +0530

    slider with progress bar completed...footersection completed...jquery event for subscribe and search icon..

--- js/custom.js
+++ js/custom.js
@@ -9,9 +9,23 @@ $(document).ready(function () {
         }
     });
 
+    // Customer Slider Progress Bar
+    var $carousel = $('.carousel');
+    var $progressBar = $('.progress');
+    var $progressBarLabel = $( '.slider__label');
+    $carousel.on('beforeChange', function(event, slick, currentSlide, nextSlide) {   
+        var calc = ( (nextSlide) / (slick.slideCount-1) ) * 100;
+        
+        $progressBar
+          .css('background-size', calc + '% 100%')
+          .attr('aria-valuenow', calc );
+        
+        $progressBarLabel.text( calc + '% completed' );
+      });
 
-
-    $('.carousel').slick({
+       // Customer Card Slider 
+    $carousel.slick({
+        centerMode: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -24,7 +38,7 @@ $(document).ready(function () {
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
-                // centerMode: true,
+                centerMode: true,
 
             }
 
@@ -51,4 +65,67 @@ $(document).ready(function () {
     });
 
 
-})
+    // Onclick Search Icon Search bar Show with close button....
+    let $search = $("#search");//nav-link search icon
+    let $close = $("#close");//nav-link close icon display none 
+    let $searchBar = $("#search-bar")// list-item
+    let $form = $("#srcForm"); //search bar modal form
+
+    $search.on('click', function(){
+        $form.css('display', 'block');
+        $searchBar.addClass("show");
+    });
+
+    $close.on('click', function(){
+        $form.css('display', 'none');
+        if($searchBar.hasClass("show")){
+            $searchBar.removeClass("show");
+        }
+    });
+
+
+});
+
+
+
+
+
+
+
+// Subscribe Form Validation
+let emailNode= $("#email");
+let subBtn = $("#sub-btn")
+let errorNode = $("#error");
+let border1 ="1px solid #f00";
+let border2 ="1px solid #0f0";
+
+
+
+function validate(){
+    errorNode.text("");
+    let email  =emailNode.val();
+    let subStr = email.substring(email.indexOf('@')+1);
+    console.log(subStr);
+
+    if(email==""){
+        errorNode.text("this field is required");
+        emailNode.css("border",border1);
+        return false;
+    }
+    else if(!email.includes('@') || subStr == ''){
+        errorNode.text("invalid email id...");
+        emailNode.css("border",border1);
+        return false;
+    }
+    else{
+        emailNode.css("border",border2);
+        return true;
+    }
+    
+}
+
+function validateForm(){
+    let st1 = validate();
+    return st1;
+    
+}
